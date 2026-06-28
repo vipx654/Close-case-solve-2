bf3c3362c3ab0b222ce502a1a22874e042481e6b
Vipul kumar <vipx654-org@users.noreply.huggingface.co>
2026-06-28 01:27:10 +0000

    Add emoji reactions + 5min auto-delete: plugins/p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -65,7 +65,7 @@ async def save_group(bot, message):
                 )
                 
         if settings["auto_delete"]:
-            await asyncio.sleep(600)
+            await asyncio.sleep(300)
             await (temp.MELCOW['welcome']).delete()
                 
                
