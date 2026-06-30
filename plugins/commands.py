40cb828f6df9745df69e674d12f4bb58c4f75685
Vipul kumar <vipx654-org@users.noreply.huggingface.co>
2026-06-30 18:23:05 +0000

    Update plugins/commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,11 +33,11 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.GSTART_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
-        if not await db.get_chat(message.chat.id):
+                if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
-                return
+        return
     if message.from_user.id not in ADMINS:
         await message.reply_text(
             "<b>⚠️ ᴘᴍ ᴅɪꜱᴀʙʟᴇᴅ !\n\nʏᴏᴜ ᴄᴀɴɴᴏᴛ ᴜꜱᴇ ᴛʜɪs ʙᴏᴛ ɪɴ ᴘᴍ.\n\n📝 ᴘʟᴇᴀꜱᴇ ꜱᴇᴀʀᴄʜ ꜰᴏʀ ꜰɪʟᴇs ᴛʜʀᴏᴜɢʜ ᴏᴜʀ ɢʀᴏᴜᴘ.</b>",
@@ -47,6 +47,7 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
+    if not await db.is_user_exist(message.from_user.id):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
